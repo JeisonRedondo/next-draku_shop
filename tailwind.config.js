74c28a599fd2e0d403494ff4441a5327efeeceeb
JeisonR <jeida16@yahoo.es>
2023-11-10 20:33:00 -0500

    [Add.Change](Class 5)Agregando archivo jsconfig para paths, cambiando paths modificados

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors');
 
 
 module.exports = {
-  purge: ['./src/**/*{html,js}'],
+  content: ['./src/**/*{html,js}'],
   theme: {
     colors:{
       ...colors,
