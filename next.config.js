bee21c210b6c2e412434ac1cb07fcc3028a76d7e
JeisonR <jeida16@yahoo.es>
2023-11-15 17:04:13 -0500

    [ADD,CHANGE]Agregando unsplash como BBDD de imagenes y modificando el dashboard para que muestre las imagenes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: { domains: ['tailwindui.com', 'images.unsplash.com'] },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
