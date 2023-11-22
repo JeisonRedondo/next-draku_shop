2ce54acbb290757d39649a753d5c57d6a03b02d5
JeisonR <jeida16@yahoo.es>
2023-11-21 19:12:30 -0500

    [ADD]Agregando paginacion generada por mi, reponde a botones por paginacion y puede aumentar o disminuir

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -11,7 +11,77 @@ const people = [
   },
 ];
 
+import useFetch from '@hooks/useFetch';
+import endPoints from '@services/api';
+import { forwardRef, useState } from 'react';
+
+const PRODUCT_LIMIT = 15;
+const PRODUCT_OFFSET = 15;
+const PAGES_AMOUNT = 3;
+const PRODUCT_QUANTITY = PRODUCT_LIMIT / PAGES_AMOUNT;
+
 export default function Dashboard() {
+  //get Products
+  const products = useFetch(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET));
+
+  //Separate products to present in layout
+  /* CONSOLE*/ console.log('Products: ', products); // Seapte
+  const separateProducts = [];
+
+  let startOf = 0;
+  let finalOf = PRODUCT_QUANTITY;
+
+  for (let index = 0; index < PAGES_AMOUNT; index++) {
+    separateProducts[index] = products?.slice(startOf, finalOf);
+    startOf = startOf + PRODUCT_QUANTITY;
+    finalOf = finalOf + PRODUCT_QUANTITY;
+  }
+  /* CONSOLE*/ console.log('Primeros 5: ', separateProducts);
+
+  const [page, setPage] = useState(0);
+  const [liveProducts, setLiveProducts] = useState(separateProducts[page]);
+
+  function changeToNextPage() {
+    if (page + 1 === PAGES_AMOUNT) {
+      alert('This is the last page');
+    } else {
+      setPage(page + 1);
+    }
+  }
+
+  function changeToPreviousPage() {
+    if (page === 0) {
+      alert('This is the first page.');
+    } else {
+      setPage(page - 1);
+    }
+  }
+
+  function changeToThisPage(page) {
+    setPage(page - 1);
+  }
+
+  function createPaginationButtons() {
+    const buttons = [];
+
+    for (let index = 0; index < PAGES_AMOUNT; index++) {
+      const pageNumber = index + 1;
+      const isActive = pageNumber === page + 1;
+
+      buttons.push(
+        <button
+          className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 disabled:bg-blue-700 disabled:text-white"
+          onClick={() => changeToThisPage(pageNumber)}
+          disabled={isActive}
+        >
+          {pageNumber}
+        </button>
+      );
+    }
+
+    return buttons;
+  }
+  1;
   return (
     <>
       <div className="flex flex-col">
@@ -25,50 +95,101 @@ export default function Dashboard() {
                       Name
                     </th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Title
+                      Category
                     </th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Status
+                      Price
                     </th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Role
+                      Id
                     </th>
                     <th scope="col" className="relative px-6 py-3">
                       <span className="sr-only">Edit</span>
                     </th>
+                    <th scope="col" className="relative px-6 py-3">
+                      <span className="sr-only">Delete</span>
+                    </th>
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
-                  {people.map((person) => (
-                    <tr key={person.email}>
+                  {separateProducts[page]?.map((product) => (
+                    <tr key={`Product-item-${product.id}`}>
                       <td className="px-6 py-4 whitespace-nowrap">
                         <div className="flex items-center">
                           <div className="flex-shrink-0 h-10 w-10">
-                            <img className="h-10 w-10 rounded-full" src={person.image} alt="" />{' '}
+                            <img className="h-10 w-10 rounded-full" src={product.images[0]} alt="" />{' '}
                           </div>
                           <div className="ml-4">
-                            <div className="text-sm font-medium text-gray-900">{person.name}</div>
-                            <div className="text-sm text-gray-500">{person.email}</div>
+                            <div className="text-sm font-medium text-gray-900">{product.title}</div>
                           </div>
                         </div>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">{person.title}</div>
-                        <div className="text-sm text-gray-500">{person.department}</div>
+                        <div className="text-sm text-gray-900">{product.category.name}</div>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap">
-                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
+                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">{product.price}</span>
                       </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.role}</td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.id}</td>
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                         <a href="/edit" className="text-indigo-600 hover:text-indigo-900">
                           Edit
+                        </a>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        <a href="/edit" className="text-indigo-600 hover:text-indigo-900">
+                          Delete
                         </a>
                       </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
+            </div>
+          </div>
+          <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+            <div className="flex flex-1 justify-between sm:hidden">
+              <button className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</button>
+              <button className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</button>
+            </div>
+            <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-center">
+              <div>
+                <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+                  <button
+                    className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                    onClick={() => changeToPreviousPage()}
+                  >
+                    <span className="sr-only">Previous</span>
+                    <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path
+                        fillRule="evenodd"
+                        d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                  {createPaginationButtons()}
+                  <span
+                    className="relative inline-flex items-center px-4 py-2 text-sm 
+                    font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0"
+                  >
+                    ...
+                  </span>
+                  <button
+                    className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                    onClick={() => changeToNextPage()}
+                  >
+                    <span className="sr-only">Next</span>
+                    <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path
+                        fillRule="evenodd"
+                        d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </nav>
+              </div>
             </div>
           </div>
         </div>
