eef3f575bd73fb736fbd8a1fa32ad8ce443ab56f
JeisonR <jeida16@yahoo.es>
2023-11-28 19:22:50 -0500

    [ADD,CHANGE](Class 20)Agregando form para agregar producto, generando logica y funcionalidad

--- src/pages/dashboard/products.jsx
+++ src/pages/dashboard/products.jsx
@@ -1,6 +1,7 @@
 import { Fragment, useState } from 'react';
 import { PlusIcon } from '@heroicons/react/20/solid';
 import Modal from '@common/Modal';
+import FormProduct from '@components/FormProduct';
 
 export default function products() {
   const [open, setOpen] = useState(false);
@@ -94,7 +95,7 @@ export default function products() {
         </div>
       </div>
       <Modal open={open} setOpen={setOpen}>
-        <h2>Hola, mundo</h2>
+        <FormProduct />
       </Modal>
     </>
   );
